8293e3927fc6d3b6153a331165bd15a391101ac3
Stefaan Ternier <stefaan.ternier@gmail.com>
2016-04-11 10:29:52 +0200

    make url configurable

--- src/main/webapp/ng/js/resources/store.js
+++ src/main/webapp/ng/js/resources/store.js
@@ -1,12 +1,12 @@
 angular.module('ARLearn')
-    .factory('Store', function StoreFactory($resource, $http) {
+    .factory('Store', function StoreFactory($resource, $http, config) {
             $http.defaults.headers.common['Authorization'] = 'GoogleLogin auth=ya29.kQK7ma-ejSZk0e0BM_Kf21HZSmeNiCca7WxODi33BwmUdOR6S70cc1ybv_C9KU1qxegJ'; //Using $http we can set header also
-            return $resource('/rest/store/', {}, {
+            return $resource(config.server+'/rest/store/', {}, {
 
                 'getFeaturedByLanguage': {
                     method: 'GET',
                     isArray: false,
-                    url: '/rest/store/games/featured/lang/:lang'
+                    url: config.server+'/rest/store/games/featured/lang/:lang'
                 }
             });
         }
